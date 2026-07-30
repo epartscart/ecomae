63224277b0a708a4382aaac21cc8fc5d0f3b2f2f
epartscart <epartscart@gmail.com>
2026-07-30 10:38:28 -0700

    Add dry-run remote ASP.NET deployment automation

---
+++ aspnet/src/EcomAE.Platform/Modules/ControlPanelModule.cs
@@ -0,0 +1,33 @@
+using EcomAE.Platform.Middleware;
+using EcomAE.Platform.Migration;
+using EcomAE.Platform.Services;
+using EcomAE.Platform.Surfaces;
+using EcomAE.Platform.Routing;
+using EcomAE.Platform.Security;
+
+namespace EcomAE.Platform.Modules;
+
+public sealed class ControlPanelModule : ISurfaceModule
+{
+    public SurfaceModuleDescriptor Descriptor { get; } = new(
+        "cp",
+        "Control Panel / Super CP",
+        EcomAeRoutes.ControlPanel,
+        "cp/",
+        "shell-started",
+        [EcomAePermissions.SuperCpAccess, EcomAePermissions.TenantCpAccess]);
+
+    public void MapEndpoints(IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet(EcomAeRoutes.ControlPanelParity, (IControlPanelParityReporter reporter) => Results.Ok(reporter.BuildReport()));
+
+        foreach (var route in EcomAeRoutes.ControlPanelAliases)
+        {
+            endpoints.MapGet(route, (HttpContext context, ISurfaceShellCatalog shells) =>
+            {
+                var tenant = context.Items[TenantResolutionMiddleware.HttpContextItemKey] as TenantContext;
+                return Results.Ok(shells.Build("cp", tenant));
+            });
+        }
+    }
+}
